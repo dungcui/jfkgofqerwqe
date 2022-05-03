bf78e956712f979a2b25bd3b2e424f88e3e47322
Macbook Air M1 <macbookairm1@Macbooks-MacBook-Air.local>
2022-05-03 15:00:42 +0700

    fix reconnect

--- src/trader.js
+++ src/trader.js
@@ -1,7 +1,9 @@
 const Web3 = require("web3");
 const abi = require("../contracts/abi")
 
-const nodeUrl = "https://bsc-dataseed2.defibit.io/";
+let nodeUrl = "https://bsc-dataseed2.defibit.io/";
+let node2 = "https://bsc-dataseed1.defibit.io/"
+let tempnode = "https://bsc-dataseed1.defibit.io/";
 const Provider = require('@truffle/hdwallet-provider');
 const { Decimal } = require("decimal.js");
 var Promise = require("bluebird");
@@ -43,6 +45,7 @@ class Trader {
                 console.log("tokenPerBnb",Decimal(tokenPerBnb[1]).div(10**this.tokenDecimal));
             }
         } catch (ex) {    
+            // console.log(ex)
         }      
         if(!tokenPerBnb)
         {
@@ -50,43 +53,55 @@ class Trader {
             return;
         }            
             try{
-            let path =[];
-            /// Number BNB to BUY 0.1
-            let amountOutMin = new Decimal(tokenPerBnb[1].toString()).mul(this.bnbWantToBuy).mul((100-this.sippable)).div(100).floor();
-            path[0] = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";
-            path[1] = this.tokenAdress;
-            /// deadline = 2 min
-            console.log("amountOutMin",amountOutMin);
+                let path =[];
+                /// Number BNB to BUY 0.1
+                let amountOutMin = new Decimal(tokenPerBnb[1].toString()).mul(this.bnbWantToBuy).mul((100-this.sippable)).div(100).floor();
+                path[0] = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";
+                path[1] = this.tokenAdress;
+                /// deadline = 2 min
+                console.log("amountOutMin",amountOutMin);
+                
+                let deadline = new Decimal(Date.now()).div(1000).floor().plus(120).toString()
+                console.log("deadline",deadline);
+                const amountIn = this.bnbWantToBuy*10**18;
+                console.log("amountIn",amountIn);
+                console.log("amountOutMin",amountOutMin);
+
+                //0	amountOutMin	uint256	4526136326259845153
+                //1	path	address[]	0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c
+                                        //0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82
+                ///2	to	address	0xEc077723DC2E2b7832D21c7381A5b73f69A968B0
+                //3	deadline	uint256	1648544182
+
+
             
-            let deadline = new Decimal(Date.now()).div(1000).floor().plus(120).toString()
-            console.log("deadline",deadline);
-            const amountIn = this.bnbWantToBuy*10**18;
-            console.log("amountIn",amountIn);
-            console.log("amountOutMin",amountOutMin);
-
-            //0	amountOutMin	uint256	4526136326259845153
-            //1	path	address[]	0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c
-                                    //0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82
-            ///2	to	address	0xEc077723DC2E2b7832D21c7381A5b73f69A968B0
-            //3	deadline	uint256	1648544182
-
-
-        
-            const receipt = await this.router.methods.swapExactETHForTokensSupportingFeeOnTransferTokens(amountOutMin.toString(), path, this.address, deadline).send({ gasPrice: this.gasPrice ,gasLimit:this.gasLimit,from :this.address ,value : amountIn});
-            if(receipt.status === true){
-                console.log("buy success");
-                this.buyed = true;
-            } else {
-                console.log("failed")
-                /// try again
-            }
-        
+                const receipt = await this.router.methods.swapExactETHForTokensSupportingFeeOnTransferTokens(amountOutMin.toString(), path, this.address, deadline).send({ gasPrice: this.gasPrice ,gasLimit:this.gasLimit,from :this.address ,value : amountIn});
+                if(receipt.status === true){
+                    console.log("buy success");
+                    this.buyed = true;
+                } else {
+                    console.log("failed")
+                    /// try again
+                }
             } catch (e){
                 console.log("failed",e)
                 /// try again
-                this.provider = new Provider(this.privateKey, nodeUrl)
-                this.web3 = new Web3(this.provider) 
+                await this.reconnect()
+                await this.trade(balance);
             }
+    }
+    async reconnect(){
+        try{
+            tempnode = node2;
+            node2=nodeUrl;
+            nodeUrl=tempnode;
+            this.provider = new Provider(this.privateKey, nodeUrl)
+            this.web3 = new Web3(this.provider)  
+        }catch(ex){
+            Promise.delay(this.delayTime);
+            await this.reconnect()
+        }
+       
     }
 
 
@@ -97,12 +112,10 @@ class Trader {
             // console.log("tokenAdress",tokenAdress)
             this.web3.eth._provider.engine.on("error", async () => {
                 try{
-                    this.provider = new Provider(this.privateKey, nodeUrl)
-                    this.web3 = new Web3(this.provider)  
+                    await this.reconnect()
                     await this.trade(balance);
                 }catch(ex){
-                    this.provider = new Provider(this.privateKey, nodeUrl)
-                    this.web3 = new Web3(this.provider)  
+                    await this.reconnect() 
                     await this.trade(balance);
                 }
             });    
--- trade.js
+++ trade.js
@@ -1,3 +1,11 @@
 const trade = require('./src/trader');
+try{
+    trade.run();
+}catch(ex){
+    console.log(ex);
+    trade.run();
+}
 
-trade.run();
+
+
+
